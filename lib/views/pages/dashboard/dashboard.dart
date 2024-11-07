0d61ac0e865444614fb69d233917b990f022a583
YashC-oder <yashmehra914@gmail.com>
2024-11-07 14:48:20 +0530

    resolved dashboard merge conflict

--- lib/views/pages/dashboard/dashboard.dart
+++ lib/views/pages/dashboard/dashboard.dart
@@ -9,29 +9,6 @@ class Dashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    return GetBuilder<DashboardController>(
-        init: DashboardController(),
-        builder: (controller) {
-          return Scaffold(
-            body: IndexedStack(
-              index: controller.tabIndex.value,
-              children: [
-                const HomeScreen(),
-                OrderHistoryView(),
-                const SettingsPagex(),
-                const EditProfilePage(),
-                OrderHistoryView(),
-                const SubmitIssuePage(),
-              ],
-            ),
-            bottomNavigationBar: CustomBottomNavBar(
-              selectedIndex: controller.tabIndex.value,
-              onItemTapped: controller.changeTabIndex,
-            ),
-          );
-        });
-=======
     return GetBuilder<DashboardController>(builder: (controller) {
       return Scaffold(
         body: IndexedStack(
@@ -43,6 +20,5 @@ class Dashboard extends StatelessWidget {
         bottomNavigationBar: const BottomNavBar(), // uncomment order and profile onTap when they are created in navBar
       );
     });
->>>>>>> cd0888ae30d5bb9adcbff8225f7e6c30c0efa4a4
   }
 }
